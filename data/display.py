03265897226485962011dd43de7e3f4289f77443
lenLRX <545976176@qq.com>
2017-03-22 12:30:50 +0800

    finished translation(without graphs)

--- data/display.py
+++ data/display.py
@@ -5,7 +5,7 @@ import math
 
 from energy import *
 
-tau_list = [0.53,0.57]
+tau_list = [0.53,0.57,0.61,0.65,0.69,0.75,0.78,0.8,0.83,0.85,1,1.2,1.35,1.45]#
 
 tau_velo = {}
 
@@ -61,6 +61,7 @@ def visualize_file(base_dir,fname,radius,tau):
     if(z_critical > 0):
         tau_velo[str(tau)].append((radius,z_critical,x_critical))
     
+    '''
     fig = plt.figure()
     line_x_velo, = plt.plot(x_arr,x_velo,label = "x_velo")
     line_z_velo, = plt.plot(x_arr,z_velo,label = "z_velo")
@@ -73,6 +74,7 @@ def visualize_file(base_dir,fname,radius,tau):
     bottom_density, = plt.plot(x_arr,btm_density)
     plt.title("R = %d density"%radius)
     plt.savefig(os.path.join(os.path.join(base_dir,img_dir),"R%d_density.jpg"%radius))
+    '''
 
 def visualize_dir(base_dir,dir_name,radius,tau):
     files = os.listdir(dir_name)
@@ -81,6 +83,7 @@ def visualize_dir(base_dir,dir_name,radius,tau):
     
     for fname in files:
         flist = fname.split("_")
+        #print flist
         if len(flist) > 2 and int(flist[2]) > max_num:
             max_num = int(flist[2])
             max_file = fname
@@ -140,14 +143,14 @@ def summary():
         legend_names.append("x_velo tau = %s"%str(tau))
         legend_names.append("theoretical_velo tau = %s"%str(tau))
     
-    plt.legend(legend_handles,legend_names,loc = 0)
+    #plt.legend(legend_handles,legend_names,loc = 0)
     plt.savefig("dimesionless.jpg")
 
 
 def visualize_all(tau_list):
     for tau in tau_list:
         tau_velo[str(tau)] = []
-        print tau_velo
+        #print tau_velo
         visualize(os.path.join(".","varR.tau" + str(tau)),tau)
     summary()
 
